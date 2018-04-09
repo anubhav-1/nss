f5a9b3dc602402592913f5b9c629eb5bbf2f162c
Anubhav Gupta <anubhav.gupta97@gmail.com>
2018-04-09 18:42:01 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>NSS</title>
+	<meta name="description" content="NSS CCET, Chandigarh">
+    <meta name="keywords" content="NSS, CCET, Chandigarh, NSS Chandigarh, Social Service">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+
+
+
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	
+</body>
+</html>
