40c5eec0bebed7d6bc13c31049ed17f51d100b45
Anubhav Gupta <anubhav.gupta97@gmail.com>
2018-04-09 19:02:15 +0530

    Initial Commit

--- index.php
+++ index.php
@@ -8,13 +8,24 @@
     <meta name="keywords" content="NSS, CCET, Chandigarh, NSS Chandigarh, Social Service">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="style.css">
-
+	
 
 
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+	<?php 
+		include 'nav.php'
+	?>
+	<section class="container">
+		<section class="row">
+			<section id="col-lg-7 col-md-6 col-sm-10">
+				<p> Test Data</p>
+			</section>
+			
+		</section>
+	</section>
 	
 </body>
 </html>
